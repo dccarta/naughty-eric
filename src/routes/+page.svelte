9b36fc9f4b3fba2b0062dd1baef3287f7e6de319
David Carter <dccarta@gmail.com>
2023-12-24 19:19:40 +0000

    intro stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,49 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { tweened } from 'svelte/motion';
+
+	let original = 10 * 60; // TYPE NUMBER OF SECONDS HERE
+	let timer = tweened(original);
+
+	// ------ dont need to modify code below
+	setInterval(() => {
+		if ($timer > 0) $timer--;
+	}, 1000);
+
+	$: minutes = Math.floor($timer / 60);
+	$: seconds = Math.floor($timer - minutes * 60);
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Naughty Eric" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Flat_Eric_pose_2.jpg" />
+	<h1>GUESS WHOOOO!!!</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<span>{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</span>
+	<progress value={$timer / original} />
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<p>
+		Hello Reuben and Phoenix - isn't this a surprise!<br />
+		I've done it <i>again</i>, as I'm sure you've surmised:<br />
+		I've <strong>taken the prezzies!</strong> I've <strong>hid them away!</strong><br />
+		And I reckon I'll get a fair bit on eBay...
+	</p>
+	<p>
+		<i>BUT</i> as you know by now, I love a good game<br />
+		So if you can find me (which <i>would</i> be a shame)<br />
+		Then I'll give you them back and we'll call it a draw<br />
+		But can you do it in time? I'm not so sure...
+	</p>
+	<p>
+		You've got <i>10 whole minutes</i> to follow these clues<br />
+		Before I do what the Grinch <i>almost</i> did to those <i>Who</i>s!<br />
+		Simply enter each code to unlock the next clue<br />
+		Are you ready? Then let's do it - <b>I'm waiting for you!</b>
+	</p>
+	<button class="">BEGIN</button>
 </section>
 
 <style>
@@ -41,19 +59,28 @@
 		width: 100%;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	p {
+		width: 50%;
 	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	button {
+		--primary: #be0b00;
+		--primary-text: #fff;
+		--secondary: #775655;
+		--secondary-text: #fff;
+		--neutral: #f4dddc;
+		--neutral-text: #524342;
+		--focus-ring: #857372;
+		background-color: #be0b00;
+		color: #fff;
+		font-weight: bold;
+		text-transform: uppercase;
+		outline: 0;
+		border: none;
+		border-radius: 6px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.199);
+		transition: box-shadow scale 100ms ease-in;
+		cursor: pointer;
+		font-size: 2rem;
 	}
 </style>
